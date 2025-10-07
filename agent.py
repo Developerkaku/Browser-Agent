8d09254733e15cdc1b8aae7643057576e08a7f10
Karthik <karthikprofessional2404@gmail.com>
2025-10-07 15:19:49 +0530

    build failing in render

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 agent = Agent(
-    task="open youtube",
+    task="open lmarena web site, if it asks to sign wait bbruntill i sign in, if does not then continue, ater opening the site give the ai in the web site a prompt asking what version is it using , what are it's capabilities and let user take control",
     llm=ChatGoogle(model="gemini-flash-latest"),
     # browser=Browser(use_cloud=True),  # Uses Browser-Use cloud for the browser
 )
