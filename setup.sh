4453c8143cf6e20b75be761ee701d0f3744f68c8
Karthik <140108316+Developerkaku@users.noreply.github.com>
2025-10-07 15:43:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ set -e
 python -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt
-python -m playwright install
+pip install uv
+uv pip install browser-use
+uvx playwright install chromium --with-deps --no-shell
